20ddc625c4cf8ab5ab648fd7420c10460758e8fe
Mojtaba Amini <mojtaba.amini.1995@gmail.com>
2020-12-19 20:31:07 +0330

    Add files via upload
    The makecashmatrix has been updated

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+      #set the inverse default value
+      r <- NULL
+      # it will be used for setting nec variable to matrix and reset inverse
+    set <- function(y) {
+      x <<- y
+      r <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(mean) r <<- inverse
+    getinverse <- function() r
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  
+  
 
 }
 
